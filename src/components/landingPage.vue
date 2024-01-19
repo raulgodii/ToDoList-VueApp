1628f056f98da903067c85636c3bd77bcba8042e
raulgodii <raulgodii13@gmail.com>
2024-01-19 13:04:50 +0100

    landing

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-    <h1>componente landing page</h1>
+    <div class="hero">
+        <h1>Your Ultimate Task Management Solution</h1>
+        <h2>Unlock the Power to Seamlessly Organize Your Daily Routine</h2>
+        <a href="" class="btn-get-started">Get Started</a>
+    </div>
+    
 </template>
