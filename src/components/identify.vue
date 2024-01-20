4b24cde82b5c99501909583d1a37a13758f9792a
raulgodii <raulgodii13@gmail.com>
2024-01-20 21:35:29 +0100

    github

--- src/components/identify.vue
+++ src/components/identify.vue
@@ -5,11 +5,11 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { GithubAuthProvider } from 'firebase/auth';
 
 let router = useRouter();
 
-function iniciarSesion() {
-    console.log("entra");
+function iniciarSesionGoogle() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
         .then((result) => {
@@ -104,6 +104,24 @@ function validarEmail(email) {
     return regexEmail.test(email);
 }
 
+function iniciarSesionGithub() {
+
+    const githubProvider = new GithubAuthProvider();
+
+    signInWithPopup(auth, githubProvider)
+        .then((result) => {
+            // El usuario ha iniciado sesión correctamente
+            errorLog.value = false;
+            const user = result.user;
+            console.log('Usuario autenticado con GitHub:', user);
+        })
+        .catch((error) => {
+            errorLog.value = true;
+            console.error('Error al iniciar sesión con GitHub:', error.message);
+        });
+
+}
+
 </script>
 
 <template>
@@ -172,7 +190,7 @@ function validarEmail(email) {
             <p class="p line">Or With</p>
 
             <div class="flex-row">
-                <button @click.prevent="iniciarSesion" class="btn google">
+                <button @click.prevent="iniciarSesionGoogle" class="btn google">
                     <svg version="1.1" width="20" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
                         style="enable-background:new 0 0 512 512;" xml:space="preserve">
@@ -193,23 +211,11 @@ function validarEmail(email) {
 
                     Google
 
-                </button><button class="btn apple">
-                    <svg version="1.1" height="20" width="20" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22.773 22.773"
-                        style="enable-background:new 0 0 22.773 22.773;" xml:space="preserve">
-                        <g>
-                            <g>
-                                <path
-                                    d="M15.769,0c0.053,0,0.106,0,0.162,0c0.13,1.606-0.483,2.806-1.228,3.675c-0.731,0.863-1.732,1.7-3.351,1.573 c-0.108-1.583,0.506-2.694,1.25-3.561C13.292,0.879,14.557,0.16,15.769,0z">
-                                </path>
-                                <path
-                                    d="M20.67,16.716c0,0.016,0,0.03,0,0.045c-0.455,1.378-1.104,2.559-1.896,3.655c-0.723,0.995-1.609,2.334-3.191,2.334 c-1.367,0-2.275-0.879-3.676-0.903c-1.482-0.024-2.297,0.735-3.652,0.926c-0.155,0-0.31,0-0.462,0 c-0.995-0.144-1.798-0.932-2.383-1.642c-1.725-2.098-3.058-4.808-3.306-8.276c0-0.34,0-0.679,0-1.019 c0.105-2.482,1.311-4.5,2.914-5.478c0.846-0.52,2.009-0.963,3.304-0.765c0.555,0.086,1.122,0.276,1.619,0.464 c0.471,0.181,1.06,0.502,1.618,0.485c0.378-0.011,0.754-0.208,1.135-0.347c1.116-0.403,2.21-0.865,3.652-0.648 c1.733,0.262,2.963,1.032,3.723,2.22c-1.466,0.933-2.625,2.339-2.427,4.74C17.818,14.688,19.086,15.964,20.67,16.716z">
-                                </path>
-                            </g>
-                        </g>
-                    </svg>
+                </button><button class="btn apple" @click.prevent="iniciarSesionGithub()">
+                <svg width="20px" x="0px" y="0px" viewBox="0 0 100 100"
+                          xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg> 
 
-                    Apple
+                    GitHub
 
                 </button>
             </div>
