10d3b20c279c1fa34911a4ed1fcf20294e4f13a5
raulgodii <raulgodii13@gmail.com>
2024-01-20 10:05:18 +0100

    sign in

--- src/components/signUp.vue
+++ src/components/signUp.vue
@@ -1,4 +1,70 @@
 <script setup>
+import { ref } from 'vue';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+let emailReq = ref(false);
+let emailValid = ref(false);
+let passReq = ref(false);
+let passMatch = ref(false);
+let passLength = ref(false);
+let email;
+let pass1;
+let pass2;
+
+function registro() {
+    if (!email) {
+        emailReq.value = true;
+    } else if(!validarEmail(email)){
+        emailValid.value = true;
+        emailReq.value = false;
+    } else {
+        emailValid.value = true;
+        emailValid.value = false;
+    }
+    if (!pass1 || !pass2) {
+        passReq.value = true;
+    } else {
+        console.log("p1: " + pass1 + "  p2: " + pass2);
+        passReq.value = false;
+        if (pass1 !== pass2) {
+            passMatch.value = true;
+        } else if(pass1.length < 6){
+            passLength.value = true;
+        } else {
+            passLength.value = false;
+            passMatch.value = false;
+        }
+    }
+    if (!emailReq.value && !passReq.value && !passMatch.value) {
+        console.log("entra");
+
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, pass1)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log("registrado");
+                // ...
+            })
+            .catch((error) => {
+                emailValid.value=true;
+                // Manejo de errores específico
+                console.error('Error al registrar usuario:', error.message);
+                // Puedes imprimir el objeto de error completo para obtener más detalles
+                console.error('Error completo:', error);
+                // ..
+            });
+    }
+}
+
+function validarEmail(email) {
+  // Expresión regular para validar direcciones de correo electrónico
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Usamos test() para verificar si el email coincide con la expresión regular
+  return regexEmail.test(email);
+}
 
 </script>
 
@@ -16,8 +82,15 @@
                         </path>
                     </g>
                 </svg>
-                <input type="text" class="input" placeholder="Enter your Email">
+                <input type="email" class="input" placeholder="Enter your Email" v-model="email">
             </div>
+            <div class="flex-column" v-if="emailReq">
+                <label style="color: red;">Email required</label>
+            </div>
+            <div class="flex-column" v-if="emailValid">
+                <label style="color: red;">Email not valid</label>
+            </div>
+            <br>
 
             <div class="flex-column">
                 <label>Password </label>
@@ -31,14 +104,42 @@
                         d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0">
                     </path>
                 </svg>
-                <input type="password" class="input" placeholder="Enter your Password">
+                <input type="password" class="input" placeholder="Enter your Password" v-model="pass1">
                 <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z">
                     </path>
                 </svg>
             </div>
-            <button class="button-submit">Sign Up</button>
+            <div class="flex-column">
+                <label>Repeat Password</label>
+            </div>
+            <div class="inputForm">
+                <svg height="20" viewBox="-64 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0">
+                    </path>
+                    <path
+                        d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0">
+                    </path>
+                </svg>
+                <input type="password" class="input" placeholder="Enter your Password" v-model="pass2">
+                <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z">
+                    </path>
+                </svg>
+            </div>
+            <div class="flex-column" v-if="passReq">
+                <label style="color: red;">Password required</label>
+            </div>
+            <div class="flex-column" v-if="passMatch">
+                <label style="color: red;">Passwords do not match</label>
+            </div>
+            <div class="flex-column" v-if="passLength">
+                <label style="color: red;">Password must be at least 6 characters</label>
+            </div>
+            <button class="button-submit" @click.prevent="registro()">Sign Up</button>
 
         </form>
     </div>
