bead7f444cc1143a7f260c726d16676392ca1410
raulgodii <raulgodii13@gmail.com>
2024-01-21 08:20:31 +0100

    404

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,7 +4,7 @@
 
 <template>
     <footer>
-        <p>© Raúl González | 2023</p>
+        <p>© Raúl González | 2024</p>
         <p>Open code in <a href="#" style="color: rgb(184, 114, 17);">GitHub</a></p>
     </footer>
 </template>
